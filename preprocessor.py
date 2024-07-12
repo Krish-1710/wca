da816ad77c67fa438fe5cdd61ab2b25a449f2633
Krish-1710 <165011242+Krish-1710@users.noreply.github.com>
2024-07-12 21:42:20 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -8,7 +8,7 @@ def preprocess(data):
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %I:%M %p - ')
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p - ')
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
